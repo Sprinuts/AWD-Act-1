ee2564e8ac084091e76b80845772bc9e3a522c3b
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2024-01-13 14:39:56 +0800

    Add files via upload

--- Bell Notification/JS/script.js
+++ Bell Notification/JS/script.js
@@ -1,11 +1,12 @@
-const button = document.getElementById('button');
-const notification = document.getElementById('notification');
+const button = document.getElementById('_button');
+const notification = document.getElementById('_notification');
 
 button.addEventListener('click', () => {
     const counter = Number(notification.getAttribute('data-count')) || 0;
 
     notification.setAttribute('data-count', counter + 1);
     notification.classList.add('count');
+    notification.classList.add('notify');
 });
 
 notification.addEventListener('animationend', () => {
